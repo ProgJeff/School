34e67722865c407f3f150e5f81d0ca13bc7f0342
ProgJeff <112576594+ProgJeff@users.noreply.github.com>
2022-12-05 08:58:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo_structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne UnePersonne = new Personne();
+            LigneFacture ligneFacture = new LigneFacture();
+
+            UnePersonne.Age = 50;
+            UnePersonne.FirstName = "Jeff";
+            UnePersonne.Name = "Marceau";
+            Console.WriteLine(UnePersonne.Name);
+            Console.WriteLine(UnePersonne.FirstName);
+            Console.Write(UnePersonne.Age);
+        }
+    }
+}
---
+++ structures.cs
@@ -0,0 +1,7 @@
+﻿public struct Personne
+{
+    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public int Age { get; set; }
+
+}
